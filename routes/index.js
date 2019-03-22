6cb4ba7bf1a5b7aa7dfab7db52ed8370b1fe6884
AymanDev <maxsa21@gmail.com>
2019-03-22 22:32:35 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,7 @@ const DBLoader = require('../src/DBLoader');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // const user = new DBLoader.User({
-  //   username: 'Test',
-  //   password: 'testpassword'
-  // });
-
-  // DBLoader.User.findOne({username: 'Test'}, (err, result) => {
-  //   if (err) {
-  //     return res.json({
-  //       message: 'User wasn\'t finded!'
-  //     });
-  //   }
-  //
-  //   return res.json({
-  //     res: result
-  //   });
-  // });
-  // user.save().then(() => {
-  //   return res.json({
-  //     message: 'db document saved'
-  //   });
-  // }).catch(() => {
-  //   return res.json({
-  //     message: 'db document save error'
-  //   });
-  //
-  //   res.json({
-  //     resultMsg: 'work'
-  //   });
-  // });
+  res.json('Hello!');
 });
 
 module.exports = router;
