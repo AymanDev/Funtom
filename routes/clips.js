eefb2c977cf08c7cb97d322dd984ace454f0f21d
AymanDev <maxsa21@gmail.com>
2019-03-23 00:45:32 +0300

    Update clips.js

--- routes/clips.js
+++ routes/clips.js
@@ -7,23 +7,26 @@ router.post('/', (req, res, next) => {
   const y = req.body.y;
   const r = 0.0002;
   const clips = [];
-
+  let msg = '';
   DBLoader.Clip.find({}, (err, results) => {
     results.forEach(clip => {
       const xx0 = Math.abs(clip.position.x - x);
       const yy0 = Math.abs(clip.position.y - y);
+      msg += clip + '\n';
+      msg += xx0 + '/' + yy0;
       if (xx0 <= r && yy0 <= r) {
         clips.push(clip);
+        msg += '/true';
       }
     });
 
-    return res.json({
-      clips
-    });
+    // return res.json({
+    //   clips
+    // });
   });
 
   return res.json({
-    message: 'Error!'
+    message: msg
   });
 });
 
