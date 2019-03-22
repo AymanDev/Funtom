d044d6efe4003519dfd0ea91ead001f87dc04aa5
AymanDev <maxsa21@gmail.com>
2019-03-22 21:10:32 +0300

    DB find test

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,35 @@ const DBLoader = require('../src/DBLoader');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const user = new DBLoader.User({
-    username: 'Test',
-    password: 'testpassword'
-  });
-  user.save().then(() => {
-    res.json({
-      message: 'db document saved'
-    });
-  }).catch(() => {
-    res.json({
-      message: 'db document save error'
-    });
+  // const user = new DBLoader.User({
+  //   username: 'Test',
+  //   password: 'testpassword'
+  // });
 
-    res.json({
-      resultMsg: 'work'
+  DBLoader.User.findOne({username: 'Test'}, (err, result) => {
+    if (err) {
+      return res.json({
+        message: 'User wasn\'t finded!'
+      });
+    }
+
+    return res.json({
+      res: result
     });
   });
+  // user.save().then(() => {
+  //   return res.json({
+  //     message: 'db document saved'
+  //   });
+  // }).catch(() => {
+  //   return res.json({
+  //     message: 'db document save error'
+  //   });
+  //
+  //   res.json({
+  //     resultMsg: 'work'
+  //   });
+  // });
 });
 
 module.exports = router;
