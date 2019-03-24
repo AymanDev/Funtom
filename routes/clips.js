2bfa82a3d50b2ffce3a62992b16d2b8549f36334
AymanDev <maxsa21@gmail.com>
2019-03-24 12:14:21 +0300

    Update clips.js

--- routes/clips.js
+++ routes/clips.js
@@ -6,7 +6,7 @@ router.post('/', (req, res, next) => {
   const x = req.body.x;
   const y = req.body.y;
   const friendUserId = req.body.friendUserId;
-  const r = 0.00004;
+  const r = 0.00008;
   const clips = [];
   const filter = {};
 
